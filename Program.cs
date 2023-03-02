1725a19fbe64c04c6a3f9892c44ac616ebd996f9
AngusBD <97964789+AngusBD@users.noreply.github.com>
2023-03-02 08:22:41 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
-﻿using static ch01Function;
+using System;
+using static ch01Function;
 using static ch02Function;
 using static ch03Function;
 namespace runningMain {
     internal class Program {
         static void Main(string[] args) {
+            //ch01_q1();
+            //ch01_q2();
+            //ch01_q3();
+            //ch01_q4();
+            //ch01_q5();
+            //ch02_q1();
+            //ch02_q2();
+            //ch02_q3();
+            //ch02_q4();
+            //ch02_q5();
             //ch03_q1();
-            ch03_q5();
+            //ch03_q2();
+            //ch03_q3();
+            //ch03_q4();
+            //ch03_q5();
         }
     }
 }
